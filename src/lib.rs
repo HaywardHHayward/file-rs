aa1844a67f92698f517479fa872feb8f33cd6662
HaywardHHayward <tictanic8@hotmail.com>
2024-09-16 20:22:24 -0500

    MVP

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-pub fn file() -> Result<(), Box<dyn std::error::Error>> {
-    let arguments = std::env::args_os()
-        .map(std::ffi::OsString::from)
-        .collect::<Vec<_>>();
-    if arguments.len() < 2 {
+pub fn file(argv: Vec<std::ffi::OsString>) -> Result<(), Box<dyn std::error::Error>> {
+    if argv.is_empty() {
         if let Ok(exe) = std::env::current_exe() {
             eprintln!("Not enough arguments. Usage: {} [files]", exe.display());
         } else {
@@ -10,7 +7,7 @@ pub fn file() -> Result<(), Box<dyn std::error::Error>> {
         }
         return Err(std::io::Error::from(std::io::ErrorKind::InvalidInput).into());
     }
-    for argument in arguments {
+    for argument in argv.iter() {
         print!("{}: ", argument.to_string_lossy());
         let possible_file = std::fs::read(std::path::Path::new(&argument));
         match possible_file {
@@ -57,11 +54,72 @@ fn classify_file(file: Vec<u8>) -> FileClassifications {
     if file.is_ascii() {
         return FileClassifications::Ascii;
     }
-    if file.iter().all(|c| c.is_ascii() || *c >= 0xA0u8) { 
-        return FileClassifications::Latin1;
-    }
     if std::str::from_utf8(&file).is_ok() {
         return FileClassifications::Utf8;
     }
+    if file.iter().all(|c| c.is_ascii() || *c >= 0xA0u8) {
+        return FileClassifications::Latin1;
+    }
     FileClassifications::Data
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::OsString;
+
+    use crate::file;
+
+    #[test]
+    fn no_args() {
+        assert!(file(vec![]).is_err());
+    }
+
+    #[test]
+    fn invalid_arg() {
+        assert!(file(vec![OsString::from("foo")]).is_ok());
+    }
+
+    #[test]
+    fn unreadable() {
+        assert!(file(vec![OsString::from("./test_files/noread")]).is_ok());
+    }
+
+    #[test]
+    fn test_data() {
+        assert!(file(vec![OsString::from("./test_files/data.data")]).is_ok());
+    }
+
+    #[test]
+    fn test_empty() {
+        assert!(file(vec![OsString::from("./test_files/empty")]).is_ok());
+    }
+
+    #[test]
+    fn test_iso() {
+        assert!(file(vec![OsString::from("./test_files/iso8859-1.txt")]).is_ok());
+    }
+
+    #[test]
+    fn test_ascii() {
+        assert!(file(vec![OsString::from("./test_files/ascii.txt")]).is_ok());
+    }
+
+    #[test]
+    fn test_utf8() {
+        assert!(file(vec![OsString::from("./test_files/utf8.txt")]).is_ok());
+    }
+
+    #[test]
+    fn test_all() {
+        assert!(file(vec![
+            OsString::from("foo"),
+            OsString::from("./test_files/noread"),
+            OsString::from("./test_files/data.data"),
+            OsString::from("./test_files/empty"),
+            OsString::from("./test_files/iso8859-1.txt"),
+            OsString::from("./test_files/ascii.txt"),
+            OsString::from("./test_files/utf8.txt")
+        ])
+        .is_ok());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use file::file;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    file()
+    file(
+        std::env::args_os()
+            .map(std::ffi::OsString::from)
+            .skip(1)
+            .collect::<Vec<_>>(),
+    )
 }
