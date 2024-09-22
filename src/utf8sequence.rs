a1a79c8e9000559e76ff2b5c9fae0670fa402a12
HaywardHHayward <tictanic8@hotmail.com>
2024-09-22 18:11:26 -0500

    Optimized utf8sequence

--- src/utf8sequence.rs
+++ src/utf8sequence.rs
@@ -1,8 +1,7 @@
-use std::vec::Vec;
-
 pub struct Utf8Sequence {
-    length: u8,
-    bytes: Vec<u8>,
+    full_length: u8,
+    current_length: u8,
+    bytes: [u8; 4],
 }
 
 impl Utf8Sequence {
@@ -15,18 +14,23 @@ impl Utf8Sequence {
         if Self::is_invalid(byte) {
             return None;
         }
-        let mut vec: Vec<u8> = Vec::with_capacity(length as usize);
-        vec.push(byte);
-        Some(Self { length, bytes: vec })
+        let mut bytes: [u8; 4] = [0; 4];
+        bytes[0] = byte;
+        Some(Self {
+            full_length: length,
+            current_length: 1,
+            bytes,
+        })
     }
     pub fn add_byte(&mut self, byte: u8) -> bool {
-        if self.bytes.len() + 1 > self.length as usize {
+        if self.current_length + 1 > self.full_length {
             return false;
         }
         if byte >= 0b1100_0000 || Self::is_invalid(byte) {
             return false;
         }
-        self.bytes.push(byte);
+        self.bytes[self.current_length as usize] = byte;
+        self.current_length += 1;
         true
     }
     pub const fn is_invalid(byte: u8) -> bool {
@@ -34,18 +38,18 @@ impl Utf8Sequence {
     }
     pub fn get_codepoint(&self) -> Option<u32> {
         let mut codepoint: u32 = self.bytes[0] as u32;
-        match self.length {
+        match self.full_length {
             1 => {}
             2 => codepoint ^= 0b1100_0000,
             3 => codepoint ^= 0b1110_0000,
             4 => codepoint ^= 0b1111_0000,
             _ => return None,
         }
-        if self.bytes.len() != self.length as usize {
+        if self.current_length != self.full_length {
             return None;
         }
-        for &byte in self.bytes.iter().skip(1) {
-            codepoint = (codepoint << 6) | ((byte ^ 0b10_000000) as u32)
+        for i in 1..self.full_length {
+            codepoint = (codepoint << 6) | ((self.bytes[i as usize] ^ 0b10_000000) as u32)
         }
         Some(codepoint)
     }
@@ -53,7 +57,7 @@ impl Utf8Sequence {
         let Some(codepoint) = self.get_codepoint() else {
             return false;
         };
-        match self.length {
+        match self.full_length {
             1 => codepoint <= 0x7F,
             2 => codepoint > 0x7F && codepoint <= 0x7FF,
             3 => codepoint > 0x7FF && codepoint <= 0xFFFF,
@@ -61,11 +65,11 @@ impl Utf8Sequence {
             _ => unreachable!(),
         }
     }
-    
+
     pub fn current_len(&self) -> usize {
-        self.bytes.len()
+        self.current_length as usize
     }
     pub const fn full_len(&self) -> usize {
-        self.length as usize
+        self.full_length as usize
     }
 }
