531a00bf1c80a7489d60a2fce589d7084fc2bfe2
HaywardHHayward <tictanic8@hotmail.com>
2024-09-27 17:26:39 -0500

    Made Utf8Sequence safer

--- src/utf8sequence.rs
+++ src/utf8sequence.rs
@@ -1,11 +1,5 @@
 use crate::utf::*;
 
-pub struct Utf8Sequence {
-    full_length: u8,
-    current_length: u8,
-    bytes: [u8; 4],
-}
-
 enum Utf8Type {
     Ascii([u8; 1]),
     Western([u8; 2]),
@@ -13,12 +7,12 @@ enum Utf8Type {
     Other([u8; 4]),
 }
 
-pub struct Utf8Sequence2 {
+pub struct Utf8Sequence {
     utf8_type: Utf8Type,
     current_length: u8,
 }
 
-impl Utf for Utf8Sequence2 {
+impl Utf for Utf8Sequence {
     type Point = u8;
     type Codepoint = u32;
     #[inline]
@@ -65,7 +59,7 @@ impl Utf for Utf8Sequence2 {
     }
 }
 
-impl Utf8Sequence2 {
+impl Utf8Sequence {
     pub const fn build(byte: u8) -> Option<Self> {
         if (byte >= 0x80 && byte <= 0xBF) || Self::is_invalid(byte) {
             return None;
@@ -110,7 +104,7 @@ impl Utf8Sequence2 {
             }
         }
     }
-    const fn get_mut(&mut self, index: usize) -> Option<&mut u8> {
+    fn get_mut(&mut self, index: usize) -> Option<&mut u8> {
         match self.utf8_type {
             Utf8Type::Ascii(ref mut value) => {
                 if index < value.len() {
@@ -151,80 +145,5 @@ impl Utf8Sequence2 {
     }
     pub const fn current_len(&self) -> usize {
         self.current_length as usize
-    }
-}
-
-impl Utf for Utf8Sequence {
-    type Point = u8;
-    type Codepoint = u32;
-
-    #[inline]
-    fn get_codepoint(&self) -> Self::Codepoint {
-        let mut codepoint: u32 = self.bytes[0] as u32;
-        match self.full_length {
-            1 => {}
-            2 => codepoint ^= 0b1100_0000,
-            3 => codepoint ^= 0b1110_0000,
-            4 => codepoint ^= 0b1111_0000,
-            _ => unreachable!(),
-        }
-        for i in 1..self.full_length {
-            codepoint = (codepoint << 6) | ((self.bytes[i as usize] ^ 0b10_000000) as u32);
-        }
-        codepoint
-    }
-    #[inline]
-    fn add_point(&mut self, point: Self::Point) -> bool {
-        if self.current_length + 1 > self.full_length {
-            return false;
-        }
-        if point >= 0b1100_0000 || Self::is_invalid(point) {
-            return false;
-        }
-        self.bytes[self.current_length as usize] = point;
-        self.current_length += 1;
-        true
-    }
-    #[inline]
-    fn is_valid(&self) -> bool {
-        let codepoint = self.get_codepoint();
-        if !is_valid_codepoint(codepoint) {
-            return false;
-        }
-        match self.full_length {
-            1 => (..=0x7F).contains(&codepoint),
-            2 => (0x80..=0x7FF).contains(&codepoint),
-            3 => (0x800..=0xFFFF).contains(&codepoint),
-            4 => (0x100000..=0x10FFFF).contains(&codepoint),
-            _ => unreachable!(),
-        }
-    }
-}
-
-impl Utf8Sequence {
-    pub const fn build(byte: u8) -> Option<Self> {
-        let length = match byte.leading_ones() {
-            0 => 1,
-            n @ 2..=4 => n,
-            _ => return None,
-        } as u8;
-        if (byte >= 0x80 && byte <= 0xBF) || Self::is_invalid(byte) {
-            return None;
-        }
-        let bytes: [u8; 4] = [byte, 0, 0, 0];
-        Some(Self {
-            full_length: length,
-            current_length: 1,
-            bytes,
-        })
-    }
-    const fn is_invalid(byte: u8) -> bool {
-        matches!(byte, 0xC0 | 0xC1 | 0xF5..)
-    }
-    pub const fn current_len(&self) -> usize {
-        self.current_length as usize
-    }
-    pub const fn full_len(&self) -> usize {
-        self.full_length as usize
     }
 }
